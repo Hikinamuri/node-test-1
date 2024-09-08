0025c8a0d0898cdf90e4da60f18a8a4c5a58f475
Hikinamuri <Hikinamuri@mail.ru>
2024-09-09 02:11:02 +0300

    added reg page template

--- frontend/src/pages/Reg/Reg.tsx
+++ frontend/src/pages/Reg/Reg.tsx
@@ -1,7 +1,42 @@
+import { useState } from "react"
+
 export const Reg = () => {
+    const [userData, setUserData] = useState({
+        name: "",
+        email: "",
+        password: "",
+    })
+
+    const inputChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+        console.log(event.target.value, event.target.name)
+        const targetName = event.target.name
+        const targetValue = event.target.value
+        
+        setUserData(prevState => ({
+            ...prevState,
+            [targetName]: targetValue
+        }))
+    }
+
+    const logUserData = (event: React.MouseEvent<HTMLButtonElement>) => {
+        event.preventDefault()
+        console.log(userData)
+    }
+
+    const saveUser = () => {
+        fetch('http://localhost:5172/api/v1/getUser/')
+
+    }
+
     return (
         <div>
-            <p>Регистрация</p>
+            <p>Авторизация</p>
+            <form action="" onSubmit={saveUser}>
+                <input type="text" name='name' value={userData.name} onChange={inputChange}/>
+                <input type="text" name='email' value={userData.email} onChange={inputChange}/>
+                <input type="text" name='password' value={userData.password} onChange={inputChange}/>
+                <button onClick={logUserData}>Click</button>
+            </form>
         </div>
     )
 }
